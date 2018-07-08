f28dc3863cd8a3654bca1f2d6a3452304488c2d0
wanghongen <329016414@qq.com>
2018-07-08 20:53:46 +0800

    20180707

--- src/main/java/com/wanghongen/demo/test/Test.java
+++ src/main/java/com/wanghongen/demo/test/Test.java
@@ -1,5 +1,10 @@
 package com.wanghongen.demo.test;
 
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by wang on 2018/7/4
  */
@@ -7,6 +12,7 @@ public class Test {
 
   public static void main(String[] args) {
     Singleton.INSTANCE.whateverMethod();
+    Map map = Collections.synchronizedMap(new HashMap<>());
   }
 
 }
