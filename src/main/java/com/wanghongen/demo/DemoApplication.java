0fdfeaefe65b9aa7311991971d7472793c0e6181
wanghongen <329016414@qq.com>
2018-08-14 23:13:04 +0800

    jpa

--- src/main/java/com/wanghongen/demo/DemoApplication.java
+++ src/main/java/com/wanghongen/demo/DemoApplication.java
@@ -7,10 +7,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
-//@SpringBootApplication
+//@Configuration
+//@EnableAutoConfiguration
+//@ComponentScan
+@SpringBootApplication
 public class DemoApplication {
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
