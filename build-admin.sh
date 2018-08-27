910e868af3ef0724b5998fcf7d1da40fb2ca6fd1
wanghongen <329016414@qq.com>
2018-08-27 22:43:33 +0800

    add

--- build-admin.sh
+++ build-admin.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
 datename=$(date +%Y%m%d-%H%M%S)
-appserver=l-db-server1.prod.hb1.ecs
-appname=small-admin-api
+#appserver=l-db-server1.prod.hb1.ecs
+appserver=/home/wanghongen/test
+appname=spring-boot-demo
 appjar=$appname.jar
-buildjar=small-admin-api/target/small-admin-api-0.0.1-boot.jar
+buildjar=target/spring-boot-demo-1.0.jar
 
 set -x
 mvn package -Dmaven.test.skip=true
 
-ssh -T $appserver <<EOFB
-      cd /srv/
-      pkill -f $appjar
-      if [ -d "$appname" ]; then
-        mv ./$appname ./$appname.bak.$datename
-        # rm -rf ./$appname
-      fi
-      mkdir ./$appname
-EOFB
+#ssh -T $appserver <<EOFB
+#      cd /srv/
+#      pkill -f $appjar
+#      if [ -d "$appname" ]; then
+#        mv ./$appname ./$appname.bak.$datename
+#        # rm -rf ./$appname
+#      fi
+#      mkdir ./$appname
+#EOFB
 
-scp $buildjar l-db-server1.prod.hb1.ecs:/srv/$appname/$appjar
+#scp $buildjar l-db-server1.prod.hb1.ecs:/srv/$appname/$appjar
+cp $buildjar $appserver/$appname/$appjar
 
 echo '>>> starting'
-ssh -T $appserver <<EOFA
-    cd /srv/$appname/
+#ssh -T $appserver <<EOFA
+    cd $appserver/$appname/
 #    nohup java -Dfile.encoding=UTF-8 -Xms4g -Xmx4g -server -XX:SurvivorRatio=8 -XX:MaxMetaspaceSize=512M -XX:+UseConcMarkSweepGC -XX:CompressedClassSpaceSize=512M -XX:MaxTenuringThreshold=5 -XX:CMSInitiatingOccupancyFraction=70 -Dlogs=logs -verbose:gc -XX:+PrintGCDateStamps -XX:+PrintGCDetails -Xloggc:logs/gc.log.$(date +%Y%m%d%H%M) -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=1 -XX:GCLogFileSize=512M -jar $appjar > nohub.log 2>&1 &
     nohup java -Dfile.encoding=UTF-8 -jar $appjar > nohub.log 2>&1 &
-EOFA
+#EOFA
 
 sleep 30
 set +x
