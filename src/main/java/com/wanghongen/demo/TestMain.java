b514f7d6adfaed24207766091be945472d70b6f3
wanghongen <wanghongen@e-sscard.com>
2020-06-15 16:33:42 +0800

    init

---
+++ src/main/java/com/wanghongen/demo/TestMain.java
@@ -0,0 +1,8 @@
+package com.wanghongen.demo;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        System.out.println("AAAAAAAAAAAAAAAAAAAA");
+    }
+}
--- src/main/java/com/wanghongen/demo/controller/HelloController.java
+++ src/main/java/com/wanghongen/demo/controller/HelloController.java
@@ -1,27 +1,27 @@
-package com.wanghongen.demo.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HelloController {
-
-
-  @GetMapping("/hello")
-  public String hello() {
-    return "hello";
-  }
-  @PostMapping("/hello")
-  public String hello2() {
-    return "post hello";
-  }
-
-  @GetMapping("/aa")
-  public String index() {
-    System.out.println();
-    return "王红恩：Hello World!SSSSeeeeeeeeeeeeee";
-  }
-
-
-}
+//package com.wanghongen.demo.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class HelloController {
+//
+//
+//  @GetMapping("/hello")
+//  public String hello() {
+//    return "hello";
+//  }
+//  @PostMapping("/hello")
+//  public String hello2() {
+//    return "post hello";
+//  }
+//
+//  @GetMapping("/aa")
+//  public String index() {
+//    System.out.println();
+//    return "王红恩：Hello World!SSSSeeeeeeeeeeeeee";
+//  }
+//
+//
+//}
