0ad70c5fbdb067041ca5789fa83f75a42ab5ab65
wanghongen <wanghongen@e-sscard.com>
2020-07-17 09:31:50 +0800

    aaa

--- src/main/java/com/wanghongen/demo/controller/HttpErrorController.java
+++ src/main/java/com/wanghongen/demo/controller/HttpErrorController.java
@@ -3,7 +3,7 @@ package com.wanghongen.demo.controller;
 import com.wanghongen.demo.base.BaseResult;
 import com.wanghongen.demo.base.WebResult;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
