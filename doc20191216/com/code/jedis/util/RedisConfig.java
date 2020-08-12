bbf84505021c44ae9c2afaf6888eb7df51f6a7ff
wanghongen <wanghongen@e-sscard.com>
2020-08-12 15:37:10 +0800

    rem

--- doc20191216/com/code/jedis/util/RedisConfig.java
+++
@@ -1,8 +0,0 @@
-package com.code.jedis.util;
-
-public class RedisConfig {
-    public static int MAX_IDLE = 8;
-    public static int MAX_ACTIVE = 8;
-    public static int TIME_OUT = 2000;
-    public static int RETRY_NUM = 3;
-}
--- doc20191216/com/rect3d/rect3d/TestMain.java
+++
@@ -1,4 +0,0 @@
-package com.rect3d.rect3d;
-
-public class TestMain {
-}
