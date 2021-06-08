412c698b2e816d61b5fb0d7e3140dca58d971fa6
wanghongen <wanghongen@e-sscard.com>
2021-06-08 17:49:34 +0800

    baidu.sdk

---
+++ src/main/java/com/baidu/BaiDuSDKApplication.java
@@ -0,0 +1,12 @@
+package com.baidu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BaiDuSDKApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BaiDuSDKApplication.class, args);
+    }
+}
